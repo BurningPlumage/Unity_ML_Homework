49b3ae38d4edd7c465db89a62858020f124da03f
BurningPlumage <55180708+BurningPlumage@users.noreply.github.com>
2020-04-27 15:39:35 +0800

    setUI

---
+++ ML_homework/Assets/script/backup_text.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class backup_text : MonoBehaviour
+{
+    public GameObject target_text;
+
+    public void backup_text_display()
+    {
+        if (target_text.activeSelf)
+        {
+            target_text.SetActive(false);
+        }
+        else
+        {
+            target_text.SetActive(true);
+        }
+    }
+}
