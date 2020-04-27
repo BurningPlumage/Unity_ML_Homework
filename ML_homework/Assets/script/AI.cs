300249108b02e04caf369f7c3ceb43d1043c04e9
BurningPlumage <55180708+BurningPlumage@users.noreply.github.com>
2020-04-27 17:07:46 +0800

    physet

---
+++ ML_homework/Assets/script/AI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAgents;
+using MLAgents.Sensors;
+
+public class AI : MonoBehaviour//Agent
+{
+    private Rigidbody2D rig;
+
+    public GameObject point1;
+    public GameObject point2;
+
+    public Text timecount_text;
+
+    private bool jump;
+    private bool can_jump;
+    float timecount;
+
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        timecount -= Time.deltaTime;
+        timecount_text.text = timecount.ToString("F2");
+    }
+
+    private void FixedUpdate()
+    {
+        can_jump = Physics2D.OverlapArea(point1.transform.position, point2.transform.position, LayerMask.GetMask("floor"));
+        if (can_jump) Debug.Log("y");
+    }
+
+    private void LateUpdate()
+    {
+        jump = false;
+    }
+}
