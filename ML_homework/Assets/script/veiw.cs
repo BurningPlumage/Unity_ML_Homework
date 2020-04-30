46f6db924fa2ab368e75fa5177e3434f0f8a39f8
BurningPlumage <55180708+BurningPlumage@users.noreply.github.com>
2020-04-30 10:21:30 +0800

    AI7 fin

---
+++ ML_homework/Assets/script/veiw.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class veiw : MonoBehaviour
+{
+    public GameObject camare1;
+    public GameObject camare2;
+
+
+
+    public void change_view()
+    {
+        if (camare1.activeSelf)
+        {
+            camare1.SetActive(false);
+            camare2.SetActive(true);
+        }
+        else
+        {
+            camare1.SetActive(true);
+            camare2.SetActive(false);
+        }
+    }
+}
